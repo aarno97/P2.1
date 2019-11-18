1ef8613b737fd28931ec46ea71a60c6963c301db
Aaron Arnold <pape.aaron97@icloud.com>
2019-11-17 20:09:15 -0600

    Created basic FlappyBird game
    Created using this guide: https://noobtuts.com/unity/2d-flappy-bird-game

---
+++ FlappyBird/Assets/Bird.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    // Movement speed
+    public float speed = 2; 
+    
+    // Flap force
+    public float force = 300; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fly towards the right
+        GetComponent<Rigidbody2D>().velocity = Vector2.right*speed; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Flap
+        if (Input.GetKeyDown(KeyCode.Space)) 
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force); 
+    }
+    
+    void OnCollisionEnter2D(Collision2D coll) {
+        //Restart
+        Application.LoadLevel(Application.loadedLevel); 
+    }
+}
