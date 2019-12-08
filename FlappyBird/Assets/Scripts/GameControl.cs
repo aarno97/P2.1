de42a9e46464781123f69fd4ecd1a0c6c89409e2
michael-j-taylor <michaeljtaylor15@gmail.com>
2019-12-07 18:13:44 -0600

    infinite scrolling & microphone control

---
+++ FlappyBird/Assets/Scripts/GameControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+    public float scrollSpeed = 0f;
+    // Start is called before the first frame update
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else if (instance != this) {
+            Destroy (gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ FlappyBird/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+    // Movement Speed (0 means don't move)
+    public float speed_v = 0;
+    public float speed_h = 0;
+    
+    // Switch Movement Direction every x seconds
+    public float switchTime = 2;
+    private Rigidbody2D pipe;
+
+    void Start() {
+        pipe = GetComponent<Rigidbody2D> ();
+        
+        // Initial Movement Direction
+        pipe.velocity = Vector2.up * speed_v;
+        pipe.velocity = Vector2.left * speed_h;
+        
+        // Switch every few seconds
+        InvokeRepeating("Switch", 0, switchTime);
+    }
+    
+    void Switch() {
+        GetComponent<Rigidbody2D>().velocity *= -1;
+    }
+}
